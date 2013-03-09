592c2a7619aa4a305cf1da143f3a16789c20a06f
Pascal Duez <pascal@duez.me>
2013-03-09 19:45:09 +0100

    Additions, refactoring, etc.

--- README.md
+++ README.md
@@ -2,13 +2,18 @@ ineo - Drupal 7 theming toolkit.
 ================================
 
 A collection of snippets and tools I find myself using recursively for new projects.
-It aims to make starting a new theme faster, and provides the tools/overrides to output proper HTML5 markup within Drupal 7.
+It aims to make creating a new theme faster, and output proper HTML5 markup within Drupal 7.
 
-v 0.1.0 Needs cleaning/refactoring.
+v0.1.0 Needs more cleaning/refactoring/testing.
+
+What it is:
++ A framework, a starter kit for themers to scaffold a new theme.
++ A base theme `primo` which cares about overriding Drupal 7 default markup to use HTML5. So we shouldn’t worry about those things in our theme.
++ A starter kit `ineo` with boilerplate and placeholder code.
 
 What it’s not:
-+ Although some cleaning is done, it is not a "reset" theme in itself.
-+ This is definitely not a production ready template theme to produce a nice site out of the box in just a few clicks.
++ Although some markup cleaning is done, it is not a "reset" theme in itself.
++ This is definitely not a production ready theme to produce a nice site out of the box in just a few clicks.
 + No clicky theme-settings here.
 + Most likely it will not reach drupal.org, as it is not meant to be yet another HTML4.9 contrib theme.
 
@@ -19,12 +24,14 @@ Use in conjunction with:
 + "Display Suite" http://dgo.to/ds
 + "Fences" http://dgo.to/fences
 + "jQuery Update" http://dgo.to/jquery_update
++ "Speedy" http://dgo.to/speedy
 
 
 Usage
 -----
 
-The basic strucure is "primo" as base theme, and "ineo" as starter kit.
+The basic strucure is `primo` as base theme, and `ineo` as starter kit.
+`ineo` is "prune friendly", so you should do whatever you like in there.
 
 Start a new subtheme with `make NAME="newthemename"`
 
@@ -32,6 +39,7 @@ Start a new subtheme with `make NAME="newthemename"`
 ToDo’s / To consider
 --------------------
 
++ Switch from makfile to Drush or Grunt.
 + Remove the custom Modernizr stuff and advocate the use of Modernizr module.
   https://github.com/Modernizr/Modernizr/issues/713
 + The region wrappers pruning is a bit agressive, find a more flexible system.
---
+++ ineo/templates/page.tpl.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * @file
+ * Primo theme implementation,
+ * display a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see http://drupal.org/node/1728148
+ */
+?>
+
+  <div id="page">
+
+    <header id="header" role="banner" class="clearfix">
+
+      <?php if ($logo): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      <?php endif; ?>
+
+      <?php if ($site_name || $site_slogan): ?>
+        <hgroup id="name-and-slogan">
+          <?php if ($site_name): ?>
+            <h1 id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
+            </h1>
+          <?php endif; ?>
+
+          <?php if ($site_slogan): ?>
+            <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
+          <?php endif; ?>
+        </hgroup><!-- #name-and-slogan -->
+      <?php endif; ?>
+
+      <?php print render($page['header']); ?>
+
+    </header>
+
+    <?php if ($main_menu || $secondary_menu): ?>
+      <nav role="navigation">
+        <?php print theme('links__system_main_menu', array(
+          'links' => $main_menu,
+          'attributes' => array(
+            'id' => 'main-menu',
+            'class' => array('links', 'inline', 'clearfix'),
+          ),
+          'heading' => array(
+            'text' => t('Main menu'),
+            'level' => 'h2',
+            'class' => array('element-invisible'),
+          ),
+        )); ?>
+        <?php print theme('links__system_secondary_menu', array(
+          'links' => $secondary_menu,
+          'attributes' => array(
+            'id' => 'secondary-menu',
+            'class' => array('links', 'inline', 'clearfix'),
+          ),
+          'heading' => array(
+            'text' => t('Secondary menu'),
+            'level' => 'h2',
+            'class' => array('element-invisible'),
+          ),
+        )); ?>
+      </nav>
+    <?php endif; ?>
+
+    <?php print $breadcrumb; ?>
+
+    <?php if ($messages): ?>
+      <div id="messages"><?php print $messages; ?></div>
+    <?php endif; ?>
+
+    <main role="main" class="clearfix">
+      <a id="main-content"></a>
+
+      <div id="content" class="column">
+        <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+        <?php print render($page['content']); ?>
+        <?php print $feed_icons; ?>
+      </div>
+
+      <?php if ($page['sidebar_first']): ?>
+        <aside id="sidebar-first" class="column sidebar">
+          <?php print render($page['sidebar_first']); ?>
+        </aside><!-- #sidebar-first -->
+      <?php endif; ?>
+
+      <?php if ($page['sidebar_second']): ?>
+        <aside id="sidebar-second" class="column sidebar">
+          <?php print render($page['sidebar_second']); ?>
+        </aside><!-- #sidebar-second -->
+      <?php endif; ?>
+
+    </main>
+
+    <footer id="footer" role="contentinfo">
+      <?php print render($page['footer']); ?>
+    </footer>
+
+  </div><!-- #page -->
