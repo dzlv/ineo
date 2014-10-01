118d76688736d099b410015ccc8dd8a4efe455ce
Pascal Duez <pascal@duez.me>
2014-10-01 15:27:32 +0200

    Remove opinionated/outdated stuff from ineo theme

--- ineo/js/ineo.js
+++ ineo/js/ineo.js
@@ -1,28 +1,8 @@
 
 /**
  * @file ineo theme scripts.
- *
  */
 
 (function( window, undefined ) {
-
-  // Local sandoxing.
-  var document = window.document,
-      $ = window.jQuery,
-      Drupal = window.Drupal,
-      Modernizr = window.Modernizr;
-
-  // Since we delegate this does not necessarily need to be in a behaviour ?
-  Drupal.behaviors.INEOextLinks = {
-    attach: function( context, settings ) {
-      var ext = new RegExp("^(https?:)?//(?!" + location.host + ")");
-
-      $("#content", context).delegate("a", "click", function() {
-        if ( ext.test(this.href) ) {
-          this.target = "_blank";
-        }
-      });
-    }
-  };
 
 }( this ));
